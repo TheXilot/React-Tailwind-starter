6300690a455ce8aca3b9fdd38ce4661c825bafa8
xilot <thexilot@gmail.com>
2022-11-12 21:14:49 +0100

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from "react";
-import NavBar from "./components/nav";
-import Slider from "./components/Slider";
-import SliderCard from "./components/SliderCard";
 import "./App.css";
 function App() {
   return <>myAPP</>;
